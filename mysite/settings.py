c8c13132233aefc7b65e2a743be98f55d4253909
Canberk <canberk697@gmail.com>
2021-05-30 15:29:46 -0700

    'commit'

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     
     os.path.join(BASE_DIR, "static"),
