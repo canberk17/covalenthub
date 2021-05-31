02e3f13561f65c74b98eabe3430c2f2ec96607b2
Canberk <canberk697@gmail.com>
2021-05-30 19:09:55 -0700

    'commit'

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '!p1ai_b373g3bmz-**m@%h9+0_8xm7*41etdbi+t266-mogm08'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cqthub.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
